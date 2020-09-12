13234358b9e9b4d18e6d4a7accb41bca10963ffd
AnkilP <a293pate@uwaterloo.ca>
2020-09-12 11:44:01 -0400

    subsetmex solution

--- subsetmex.cpp
+++ subsetmex.cpp
@@ -7,9 +7,52 @@
 #include <vector>
 #include <queue>
 #include <utility>
+#include <algorithm>
 
-int solution(const int & N, const std::vector<int> & nums) {
-    
+int solution(const int & N, std::vector<int> & nums) {
+    std::sort(nums.begin(), nums.end());
+    auto min1 = 0;
+    auto min2 = 0; 
+    auto pointer1 = 0;
+    auto pointer2 = 1;
+    bool mex1 = false;
+    bool mex2 = false;
+
+    if(nums.size() == 1) {
+        return 1;
+    }
+    else if(nums.size() == 0) {
+        return 0;
+    }
+    else {
+        while((pointer1 <= nums.size() && pointer2 <= nums.size()) || (mex1 == true && mex2 == true)) {
+            if(nums[pointer1] == min1 && mex1 == false){
+                int step = 1;
+                if(mex2 == true) {
+                    pointer1 = pointer1 + step;
+                }
+                pointer1 = pointer1 + step + 1;
+                min1++;
+            }
+            else {
+                //pointer1 can stop moving
+                mex1 = true;
+            }
+
+            if(nums[pointer2] == min2 && mex2 == false) {
+                int step = 1;
+                if(mex1 == true) {
+                    pointer2 = pointer2 + step;
+                }
+                pointer2 = pointer2 + step + 1;
+                min2++;
+            }
+            else {
+                mex2 = true;
+            }
+        }
+    }
+    return min1 + mn2;
 }
 
 int main(int argc, char * argv[]){
@@ -38,8 +81,8 @@ int main(int argc, char * argv[]){
         stones.clear();
         std::getline(std::cin, stones);
         std::istringstream iss2(stones);
-        std::vector<int> e(std::istream_iterator<int>(iss2), std::istream_iterator<int>());
-        auto sol = solution(N, stones);
+        std::vector<int> e(std::istream_iterator<int>(iss2), {});
+        auto sol = solution(N, e);
         std::cout << sol;
         std::cout << "\n";
     }
